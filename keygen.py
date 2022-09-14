9830f120b42631c88d8039966ff30493bae79e17
Tamas Spisak <tamas.spisak@uk-essen.de>
2022-09-14 18:36:52 +0200

    key generator

---
+++ keygen.py
@@ -0,0 +1,95 @@
+import yaml
+import glob
+import os
+import numpy as np
+
+_keys_ = np.array([
+    'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Bb', 'B', 'Cb'
+])
+
+_numerals_ = np.array([
+    'I', 'bII', 'II', 'bIII', 'III', 'IV', 'bV', 'V', 'vVI', 'VI', 'bVII', 'VII'
+])
+
+
+def transpose(degree, key):
+    if key == 'Roman Numeral':
+        return degree
+    return _keys_[(np.argwhere(_numerals_ == 'I').flatten()[0] + np.argwhere(_keys_ == 'C').flatten()[0])%12]
+
+
+for f in glob.glob('songs/*.y?ml'):
+    with open(f, 'r') as file:
+        song = yaml.safe_load(file)
+        print(song)
+
+    outfile = os.path.splitext(f)[0] + '.md'
+    print(outfile)
+    with open('docs/' + outfile, 'w') as docfile:
+        docfile.write('# ' + song['title'] + os.linesep)
+        docfile.write('' + song['description'] + os.linesep)
+        docfile.write(os.linesep)
+
+        docfile.write('##  ' + 'Videos' + os.linesep)
+        for i in song['similar']:
+            docfile.write('- ' + i['link'] + os.linesep)
+            docfile.write(' ' + i['description'] + os.linesep)
+        docfile.write(os.linesep)
+
+        docfile.write('##  ' + 'Backing Tracks' + os.linesep)
+        for i in song['backtracks']:
+            docfile.write('- ' + i['link'] + os.linesep)
+            docfile.write(' ' + i['description'] + os.linesep)
+        docfile.write(os.linesep)
+
+        for key in ['Roman Numeral'] + _keys_.tolist():
+            docfile.write('# ' + key + os.linesep)
+            for part in song['chords']:
+                docfile.write('## ' + part + os.linesep)
+                lines = song['chords'][part].split('\n')
+                # print table header
+                for i in range(len(lines[0].split())):
+                    docfile.write(' |  \t')
+                docfile.write('|' + os.linesep)
+                for i in range(len(lines[0].split())):
+                    docfile.write(' |----------\t')
+                docfile.write('|' + os.linesep)
+
+                for line in lines:
+                    for tag in line.split():
+                        tokens = tag.split(',')
+                        degree = tokens[0]
+                        if degree == '%':
+                            if len(tokens)>1:
+                                # quality doesn't change
+                                quality = ''
+                                extensions = tokens[1:]
+                        else:
+                            print(degree, key)
+                            degree = transpose(degree, key)
+                            quality = tokens[1]
+                            if len(tokens) > 2:
+                                extensions = tokens[2:]
+                        docfile.write(' | ' + degree + quality + ' ' + str(extensions) + '\t')
+                    docfile.write(' |' + os.linesep)
+                docfile.write(os.linesep + os.linesep)
+
+
+
+
+
+
+
+
+
+#
+
+#print(song)
+
+
+#with open('mkdocs.yml', 'r') as file:
+#    mkdocs_yaml = yaml.safe_load(file)
+
+#with open('names.yaml', 'w') as file:
+#    yaml.dump(yaml.safe_load(mkdocs_yaml), file)
+
