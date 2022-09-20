73cc5baacc1dcf122bf13833b1ffa1b4725599b8
Tamas Spisak <spisak.tms@gmail.com>
2022-09-20 19:17:13 +0200

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -40,7 +40,7 @@ def metronome(bpm=120, time_signature='4/4'):
     else:
         raise AssertionError('No such time signature: ' + str(time_signature))
                              
-    return "<iframe src="https://guitarapp.com/metronome-embed.html?tempo="+ str(bpm) + "&timeSignature=" + str(time_signature) + "2&pattern=1" title="Metronome" style="width: 360px; height:520px; border-style: none; border-radius: 4px;"> </iframe>
+    return '<iframe src="https://guitarapp.com/metronome-embed.html?tempo='+ str(bpm) + '&timeSignature=' + str(time_signature) + '2&pattern=1" title="Metronome" style="width: 360px; height:520px; border-style: none; border-radius: 4px;"> </iframe>'
 
 
 def transpose(degree, key):
